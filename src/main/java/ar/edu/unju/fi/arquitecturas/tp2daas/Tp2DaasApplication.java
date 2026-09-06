19b2c7edd00e35e5abdc07715ce4baafa5a74fe9
TheMiggaCool <maurosoliz505@gmail.com>
2026-09-06 20:14:32 -0300

    Initial commit

---
+++ src/main/java/ar/edu/unju/fi/arquitecturas/tp2daas/Tp2DaasApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.arquitecturas.tp2daas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2DaasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp2DaasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ar/edu/unju/fi/arquitecturas/tp2daas/Tp2DaasApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.arquitecturas.tp2daas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2DaasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
